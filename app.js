306e810dd16a6ef2d39bc4eb293291a0255322e3
Nikhil-Reddy-Karukonda <karukonda.n@northeastern.edu>
2023-10-10 15:03:59 -0400

    code refactor

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const { healthRoutes, assignmentRoutes } = require('./routes');
 
-// Importing Sequelize database connection (instance)
-const { db } = require('./models/model');
 const processCsv = require('./helpers/userImporter');
 const app = express();
 
@@ -15,8 +13,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 8080;
 
 app.use(healthRoutes);
 app.use('/v1/assignments', assignmentRoutes);
@@ -32,23 +28,5 @@ else if (process.env.ENV_TYPE === 'GITHUB_CI') {
 }
 
 processCsv(filePath);
-
-// Sync the database and start the server 
-// Set force to false to avoid dropping tables
-
-// alter: true: Adjusts database tables to match model definitions. 
-// Adds or removes columns as necessary without dropping tables. 
-// Useful for updating the database schema after changes to models
-
-db.sync({ force: false, alter: true })
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Web Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error syncing database:', error);
-        process.exit(1);  // Exit the process with failure code
-    });
 
 module.exports = app;
